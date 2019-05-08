0c05350cf989a2b223e736068cab89b35f3b2c27
DL <levai@vivaldi.net>
2019-05-08 07:12:13 +0200

    added regularizing and some visualization

--- main.py
+++ main.py
@@ -3,36 +3,38 @@ import numpy as np
 import time
 import os
 import sys
+import json
 
 import networks
 import data
 
-DATASET="mnist"
-TRAINSIZE=20000
-SEED=None
-BN_DO= "DO"  # "BN" (batchnorm), "DO" (dropout), None
-BATCH_SIZE=50
-DEPTH=3
-WIDTH=10
+DATASET = "mnist"
+TRAINSIZE = 20000
+SEED = None
+BN_DO = 'DO'  # "BN" (batchnorm), "DO" (dropout), None
+BATCH_SIZE = 100
+DEPTH = 4
+WIDTH = 20
 OUTPUT_COUNT = 10
-LR=0.001
-MEMORY_SHARE=0.25
-ITERS=10000
-AUGMENTATION=False
-SESSION_NAME="tmp_{}".format(time.strftime('%Y%m%d-%H%M%S'))
-BN_WEIGHT =  0
+LR = 0.001
+MEMORY_SHARE = 0.25
+ITERS = 10000
+AUGMENTATION = False
+SESSION_NAME = "tmp_{}".format(time.strftime('%Y%m%d-%H%M%S'))
+BN_WEIGHT = 0
 COV_WEIGHT = 0
-CLASSIFIER_TYPE = "dense" # "conv" / "dense"
+CLASSIFIER_TYPE = "dense"  # "conv" / "dense"
 LOG_DIR = "logs/%s" % SESSION_NAME
 os.system("rm -rf {}".format(LOG_DIR))
 
+
 def heuristic_cast(s):
-    s = s.strip() # Don't let some stupid whitespace fool you.
-    if s=="None":
+    s = s.strip()  # Don't let some stupid whitespace fool you.
+    if s == "None":
         return None
-    elif s=="True":
+    elif s == "True":
         return True
-    elif s=="False":
+    elif s == "False":
         return False
     try:
         return int(s)
@@ -43,6 +45,7 @@ def heuristic_cast(s):
     except ValueError:
         pass
     return s
+
 
 for k, v in [arg.split('=', 1) for arg in sys.argv[1:]]:
     assert v != '', "Malformed command line"
@@ -56,19 +59,21 @@ for k, v in [arg.split('=', 1) for arg in sys.argv[1:]]:
 
 ##########################################
 
-(X_train, y_train), (X_devel, y_devel), (X_test, y_test) = data.load_data(DATASET, SEED)
+(X_train, y_train), (X_devel, y_devel), (X_test,
+                                         y_test) = data.load_data(DATASET, SEED)
 
 X_train = X_train[:TRAINSIZE]
 y_train = y_train[:TRAINSIZE]
-
 INPUT_SHAPE = X_train.shape[1:]
-train_gen = data.classifier_generator((X_train, y_train), BATCH_SIZE, augment=AUGMENTATION)
-
+train_gen = data.classifier_generator((X_train, y_train), BATCH_SIZE,
+                                      augment=AUGMENTATION)
 inputs = tf.placeholder(tf.float32, shape=[BATCH_SIZE] + list(INPUT_SHAPE))
 if CLASSIFIER_TYPE == "dense":
-    output, activations, zs = networks.DenseNet(inputs, DEPTH, WIDTH, BN_DO, OUTPUT_COUNT, dropout=0.5)
+    output, activations, zs = networks.DenseNet(inputs, DEPTH, WIDTH, BN_DO,
+                                                OUTPUT_COUNT, dropout=1)
 elif CLASSIFIER_TYPE == "conv":
-    output, activations, zs = networks.LeNet(inputs, BN_DO, OUTPUT_COUNT, dropout=0.8)
+    output, activations, zs = networks.LeNet(
+        inputs, BN_DO, OUTPUT_COUNT, dropout=0.8)
 
 
 labels = tf.placeholder(tf.uint8, shape=[BATCH_SIZE])
@@ -79,15 +84,21 @@ loss_list = []
 xent_loss = tf.nn.softmax_cross_entropy_with_logits(
     logits=output,
     labels=labels_onehot
-    )
+)
 xent_loss = tf.reduce_mean(xent_loss)
 loss_list.append(('xent_loss', xent_loss))
 total_loss = xent_loss
 
+# Regularization
+
+reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+reg_constant = 0.01
+total_loss += reg_constant * tf.math.reduce_sum(reg_losses)
 # activations = activations[:1] # TODO
 
 cov_ops = []
-if COV_WEIGHT > 0: # push the off diagonal elements of the featurewise correlation matrix to zero
+# push the off diagonal elements of the featurewise correlation matrix to zero
+if COV_WEIGHT > 0:
     cov_loss = tf.constant(0.0)
     cov_ops = []
     for act in activations:
@@ -95,23 +106,24 @@ if COV_WEIGHT > 0: # push the off diagonal elements of the featurewise correlati
         act_centered = act - tf.reduce_mean(act, axis=0)
         for i in range(feature_count):
             for j in range(i, feature_count):
-                covariance, cov_op = tf.contrib.metrics.streaming_covariance(act_centered[i], act_centered[j])
+                covariance, cov_op = tf.contrib.metrics.streaming_covariance(
+                    act_centered[i], act_centered[j])
                 cov_ops.append(cov_op)
                 cov_loss += COV_WEIGHT * tf.square(covariance)
-    total_loss += cov_loss
-    loss_list.append(('cov_loss', cov_loss))
+                total_loss += cov_loss
+                loss_list.append(('cov_loss', cov_loss))
 
-
-if BN_WEIGHT > 0: # push each neuron to have zero mean and unit variance output
+# push each neuron to have zero mean and unit variance output
+if BN_WEIGHT > 0:
     bn_loss = tf.constant(0.0)
     for z in zs:
         z_mean = tf.reduce_mean(tf.square(z), axis=0)
         z_variance = tf.reduce_sum(tf.square(z - z_mean), axis=0)
-#        bn_loss += tf.reduce_sum(z_mean)
+        #        bn_loss += tf.reduce_sum(z_mean)
         bn_loss += tf.reduce_sum(tf.square(z_variance - 1))
-    bn_loss = BN_WEIGHT * bn_loss
-    total_loss += bn_loss
-    loss_list.append(('bn_loss', bn_loss))
+        bn_loss = BN_WEIGHT * bn_loss
+        total_loss += bn_loss
+        loss_list.append(('bn_loss', bn_loss))
 
 
 # TODO other losses
@@ -124,11 +136,12 @@ for (name, loss) in loss_list:
     loss_summaries.append(tf.summary.scalar(name, loss))
 
 # keep track of how significant activations are as reflected by the magnitude of outgoing weights
-kernels = [v for v in tf.trainable_variables() if (v.name.find("dense_") == 0) and v.name.find("kernel") > 0]
+kernels = [v for v in tf.trainable_variables() if (
+    v.name.find("dense_") == 0) and v.name.find("kernel") > 0]
 for kernel in kernels:
-        k = tf.reduce_sum(tf.abs(kernel), axis=1)
-        k = k / tf.reduce_sum(k)
-        loss_summaries.append(tf.summary.histogram(kernel.name, k))
+    k = tf.reduce_sum(tf.abs(kernel), axis=1)
+    k = k / tf.reduce_sum(k)
+    loss_summaries.append(tf.summary.histogram(kernel.name, k))
 
 merged_loss_summary_op = tf.summary.merge(loss_summaries)
 
@@ -140,10 +153,13 @@ optimizer = tf.train.AdamOptimizer(
 config = tf.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = MEMORY_SHARE
 session = tf.Session(config=config)
-print("NETWORK PARAMETER COUNT", np.sum([np.prod(v.shape) for v in tf.trainable_variables()]))
+print("NETWORK PARAMETER COUNT",
+      np.sum([np.prod(v.shape) for v in tf.trainable_variables()]))
 
 session.run(tf.global_variables_initializer())
 session.run(tf.local_variables_initializer())
+activations_dict = {}
+
 
 def evaluate(Xs, ys, BATCH_SIZE):
     nonzeros = []
@@ -155,7 +171,8 @@ def evaluate(Xs, ys, BATCH_SIZE):
     _total_losses = []
     _total_acc = []
     for X_batch, y_batch in eval_gen:
-        value_list = session.run([total_loss, output, activations] + list(cov_ops), feed_dict={inputs:X_batch, labels:y_batch})
+        value_list = session.run([total_loss, output, activations] + list(cov_ops),
+                                 feed_dict={inputs: X_batch, labels: y_batch})
         (_total_loss, predicted, _activations) = value_list[:3]
         _total_acc.append(accuracy(predicted, y_batch))
         _total_losses.append(_total_loss)
@@ -166,17 +183,18 @@ def evaluate(Xs, ys, BATCH_SIZE):
     eval_acc = np.mean(_total_acc)
     for i, _ in enumerate(nonzeros):
         nonzeros[i] = nonzeros[i] * 1.0 / (len(Xs))
-        nonzeros[i] = np.histogram(nonzeros[i], range=(0.0, 1.0))[0]
-    return eval_loss, eval_acc, nonzeros
+        nonzeros[i] = np.histogram(nonzeros[i], bins=10, range=(0.0, 1.0))[0]
+    return eval_loss, eval_acc, nonzeros, activations
+
 
 def accuracy(predicted, expected):
     return float(np.sum(np.argmax(predicted, axis=1) == expected)) / len(predicted)
+
 
 start_time = time.time()
 
 for iteration in range(ITERS+1):
     train_data = next(train_gen)
-
     # training step
     _, _total_loss, predicted, loss_summary = session.run(
         [optimizer, total_loss, output, merged_loss_summary_op],
@@ -187,10 +205,27 @@ for iteration in range(ITERS+1):
     # eval step
     if iteration % 500 == 0:
         train_acc = accuracy(predicted, train_data[1])
-        eval_loss, eval_acc, nonzeros = evaluate(X_devel, y_devel, BATCH_SIZE)
-        print("{}:\t train acc {},\t dev acc {}".format(iteration, train_acc, eval_acc))
-        print(nonzeros)
-    
-
-print("Total time: {}".format (time.time() - start_time))
-
+        eval_loss, eval_acc, nonzeros, current_activations = evaluate(
+            X_devel, y_devel, BATCH_SIZE)
+        print(_total_loss)
+        print('Total loss:{:.3f}, L reg:{}'.format(_total_loss,
+                                                   session.run(reg_losses)))
+        # print(session.run([reg_losses]))
+        # print(tf.math.reduce_sum(reg_losses))
+        print("{:>5}:    train acc {:.2f}    dev acc {:.2f}".format(
+            iteration, train_acc, eval_acc))
+        # print(zs)
+        for line in nonzeros:
+            print(' '.join(['{: <2}'.format(x) for x in line]))
+        print()
+        activations_dict[iteration] = session.run([current_activations],
+                                                  feed_dict={inputs:
+                                                             X_devel[:BATCH_SIZE],
+                                                             labels:
+                                                             y_devel[:BATCH_SIZE]})
+print("Total time: {}".format(time.time() - start_time))
+for it in activations_dict:
+    activations_dict[it] = [[nda.tolist() for nda in lst]
+                            for lst in activations_dict[it]]
+with open('neuron_logs/output_{}'.format(time.strftime('%Y%m%d-%H%M%S')), 'w') as f:
+    json.dump(activations_dict, f)
--- networks.py
+++ networks.py
@@ -1,21 +1,28 @@
 import tensorflow as tf
+import tensorflow.layers
+
 
 def DenseNet(inputs, depth, width, bn_do, output_count, dropout=0.5):
     activations = []
     zs = []
     output = tf.reshape(inputs, (inputs.shape[0], -1))
     for i in range(depth):
-        output = tf.layers.dense(output, width, name="dense_{}".format(i))
+        output = tf.layers.dense(output, width,
+                                 kernel_regularizer=tf.contrib.layers.l1_regularizer(
+                                     scale=0.01),
+                                 name="dense_{}".format(i))
         zs.append(output)
         if bn_do == "BN":
             output = tf.layers.batch_normalization(output)
         output = tf.nn.relu(output)
         activations.append(output)
-
     if bn_do == "DO":
         output = tf.nn.dropout(output, dropout)
 
-    output = tf.layers.dense(output, output_count, name="dense_{}".format(depth))
+    output = tf.layers.dense(output, output_count,
+                             kernel_regularizer=tf.contrib.layers.l1_regularizer(
+                                 scale=0.01),
+                             name="dense_{}".format(depth))
     return output, activations, zs
 
 
@@ -23,30 +30,34 @@ def LeNet(inputs, bn_do, output_count, dropout=0.5):
     activations = []
     zs = []
     output = inputs
-    output = tf.layers.conv2d(output, 6, (5,5), padding="same", name="conv_1")
+    output = tf.layers.conv2d(output, 6, (5, 5), padding="same", name="conv_1")
     zs.append(output)
     output = tf.nn.relu(output)
     activations.append(tf.contrib.layers.flatten(output))
-    output = tf.nn.max_pool(output, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    output = tf.nn.max_pool(output, ksize=[1, 2, 2, 1], strides=[
+                            1, 2, 2, 1], padding='VALID')
 
-    output =  tf.layers.conv2d(output, 16, (5,5), padding="valid", name="conv_2")
+    output = tf.layers.conv2d(
+        output, 16, (5, 5), padding="valid", name="conv_2")
     zs.append(output)
     output = tf.nn.relu(output)
     activations.append(tf.contrib.layers.flatten(output))
-    output = tf.nn.max_pool(output, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    output = tf.nn.max_pool(output, ksize=[1, 2, 2, 1], strides=[
+                            1, 2, 2, 1], padding='VALID')
 
-    output =  tf.layers.conv2d(output, 120, (5,5), padding="valid", name="conv_3")
+    output = tf.layers.conv2d(
+        output, 120, (5, 5), padding="valid", name="conv_3")
     zs.append(output)
     output = tf.nn.relu(output)
     activations.append(tf.contrib.layers.flatten(output))
 
-    output = tf.reshape(output ,[-1,120])
+    output = tf.reshape(output, [-1, 120])
 
     output = tf.layers.dense(output, 84, name="dense_1")
     output = tf.nn.relu(output)
 
     if bn_do == "DO":
         output = tf.nn.dropout(output, dropout)
-        
+
     output = tf.layers.dense(output, output_count, name="dense_2")
     return output, activations, zs
