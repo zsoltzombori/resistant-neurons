b97757a808b9947457fff90650beb1f4c746730d
levaid <levaid@vivaldi.net>
2019-11-11 13:55:53 +0100

    syncd little machine local

---
+++ manipulate.py
@@ -0,0 +1,359 @@
+import tensorflow as tf
+import numpy as np
+import time
+import os
+import sys
+import json
+import itertools
+import networks
+import data
+
+DATASET = "fashion_mnist"
+TRAINSIZE = 60000
+SEED = None
+BN_DO = None  # "BN" (batchnorm), "DO" (dropout), None
+BATCH_SIZE = 500
+DEPTH = 4
+WIDTH = 30
+OUTPUT_COUNT = 10
+LR = 0.001
+MEMORY_SHARE = 0.25
+ITERS = 500
+EVALUATION_CHECKPOINT = 20
+AUGMENTATION = False
+SESSION_NAME = "{}".format(time.strftime('%Y%m%d-%H%M%S'))
+BN_WEIGHT = 0
+COV_WEIGHT = 0
+CLASSIFIER_TYPE = "dense"  # "conv" / "dense"
+LOG_DIR = "logs/%s" % SESSION_NAME
+EVALUATE_USEFULNESS = False
+USEFULNESS_EVAL_SET_SIZE = 10000
+
+os.system("rm -rf {}".format(LOG_DIR))
+# os.nice(20)
+
+
+def heuristic_cast(s):
+    s = s.strip()  # Don't let some stupid whitespace fool you.
+    if s == "None":
+        return None
+    elif s == "True":
+        return True
+    elif s == "False":
+        return False
+    try:
+        return int(s)
+    except ValueError:
+        pass
+    try:
+        return float(s)
+    except ValueError:
+        pass
+    return s
+
+
+for k, v in [arg.split('=', 1) for arg in sys.argv[1:]]:
+    assert v != '', "Malformed command line"
+    assert k.startswith('--'), "Malformed arg %s" % k
+    k = k[2:]
+    assert k in locals(), "Unknown arg %s" % k
+    v = heuristic_cast(v)
+    print("Changing argument %s from default %s to %s" % (k, locals()[k], v))
+    locals()[k] = v
+
+
+##########################################
+
+dummy_mask = np.ones((DEPTH, WIDTH))
+# dummy_mask[0, :] = np.zeros(WIDTH)
+# dummy_mask[0, 2:5] = np.ones(3)
+
+(X_train, y_train), (X_devel, y_devel), (X_test, y_test) = data.load_data(DATASET, SEED, USEFULNESS_EVAL_SET_SIZE)
+
+X_train = X_train[:TRAINSIZE]
+y_train = y_train[:TRAINSIZE]
+INPUT_SHAPE = X_train.shape[1:]
+train_gen = data.classifier_generator((X_train, y_train), BATCH_SIZE,
+                                      augment=AUGMENTATION)
+inputs = tf.placeholder(tf.float32, shape=[BATCH_SIZE] + list(INPUT_SHAPE))
+mask = tf.placeholder(tf.float32, shape=[DEPTH, WIDTH])
+if CLASSIFIER_TYPE == "dense":
+    output, activations, zs = networks.DenseNet(inputs, DEPTH, WIDTH, BN_DO,
+                                                OUTPUT_COUNT, dropout=0.5, mask=mask)
+elif CLASSIFIER_TYPE == "conv":
+    output, activations, zs = networks.LeNet(
+        inputs, BN_DO, OUTPUT_COUNT, dropout=0.8)
+
+
+labels = tf.placeholder(tf.uint8, shape=[BATCH_SIZE])
+labels_onehot = tf.one_hot(labels, OUTPUT_COUNT)
+
+
+loss_list = []
+
+xent_loss = tf.nn.softmax_cross_entropy_with_logits(
+    logits=output,
+    labels=labels_onehot
+)
+xent_loss = tf.reduce_mean(xent_loss)
+loss_list.append(('xent_loss', xent_loss))
+total_loss = xent_loss
+
+# Regularization
+
+reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+reg_constant = 0.01
+total_loss += reg_constant * tf.math.reduce_sum(reg_losses)
+
+# push each neuron to have zero mean and unit variance output
+if BN_WEIGHT > 0:
+    bn_loss = tf.constant(0.0)
+    for z in zs:
+        z_mean = tf.reduce_mean(tf.square(z), axis=0)
+        z_variance = tf.reduce_sum(tf.square(z - z_mean), axis=0)
+        #        bn_loss += tf.reduce_sum(z_mean)
+        bn_loss += tf.reduce_sum(tf.square(z_variance - 1))
+        bn_loss = BN_WEIGHT * bn_loss
+        total_loss += bn_loss
+        loss_list.append(('bn_loss', bn_loss))
+
+
+# TODO other losses
+
+loss_list.append(('total_loss', total_loss))
+
+log_writer = tf.summary.FileWriter(LOG_DIR, graph=tf.get_default_graph())
+loss_summaries = []
+for (name, loss) in loss_list:
+    loss_summaries.append(tf.summary.scalar(name, loss))
+
+# keep track of how significant activations are as reflected by the magnitude of outgoing weights
+kernels = [v for v in tf.trainable_variables() if (
+    v.name.find("dense_") == 0) and v.name.find("kernel") > 0]
+
+for kernel in kernels:
+    k = tf.reduce_sum(tf.abs(kernel), axis=1)
+    k = k / tf.reduce_sum(k)
+    loss_summaries.append(tf.summary.histogram(kernel.name, k))
+
+merged_loss_summary_op = tf.summary.merge(loss_summaries)
+
+
+optimizer = tf.train.AdamOptimizer(
+    learning_rate=LR
+).minimize(total_loss)
+
+config = tf.ConfigProto(device_count={'GPU': 2})
+# config = tf.ConfigProto()
+# config.gpu_options.per_process_gpu_memory_fraction = MEMORY_SHARE
+session = tf.Session(config=config)
+print("NETWORK PARAMETER COUNT",
+      np.sum([np.prod(v.shape) for v in tf.trainable_variables()]))
+
+session.run(tf.global_variables_initializer())
+session.run(tf.local_variables_initializer())
+activations_dict = {}
+zs_dict = {}
+
+
+def evaluate(Xs, ys, BATCH_SIZE):
+    nonzeros = []
+    for a in activations:
+        assert len(a.shape) == 2
+        nonzeros.append(np.zeros(int(a.shape[1])))
+
+    eval_gen = data.classifier_generator((Xs, ys), BATCH_SIZE, infinity=False)
+    _total_losses = []
+    _total_acc = []
+    labels_to_return = [[], []]
+    # print('we are before entering eval_gen')
+    for X_batch, y_batch in eval_gen:
+        value_list = session.run([total_loss, output, activations],
+                                 feed_dict={inputs: X_batch, labels: y_batch, mask: dummy_mask})
+        (_total_loss, predicted, _activations) = value_list[:3]
+        # labels_to_return += [(y_batch, np.argmax(predicted, axis=1))]
+        labels_to_return[0] += [y_batch.tolist()]
+        labels_to_return[1] += [np.argmax(predicted, axis=1).tolist()]
+        _total_acc.append(accuracy(predicted, y_batch))
+        _total_losses.append(_total_loss)
+        for i, a in enumerate(_activations):
+            nonzeros[i] += np.count_nonzero(a, axis=0)
+
+    eval_loss = np.mean(_total_losses)
+    eval_acc = np.mean(_total_acc)
+    for i, _ in enumerate(nonzeros):
+        nonzeros[i] = nonzeros[i] * 1.0 / (len(Xs))
+        nonzeros[i] = np.histogram(nonzeros[i], bins=10, range=(0.0, 1.0))[0]
+
+    return eval_loss, eval_acc, nonzeros, activations, zs, labels_to_return
+
+
+def evaluate_usefulness(Xs, ys, usefulness_mask):
+    for a in activations:
+        assert len(a.shape) == 2
+
+        _total_losses = []
+        _total_acc = []
+        # VERY QUCIK AND REAL DIRTY
+        # reinitializing eval_gen between calls is very costly, so we need to make an infinite one
+        # however, we lose deterministric results this way
+        # also, we manually have to specify when to stop in this cycle below
+        # it hurts me to do this
+        # TODO change the hardcoded number
+
+        i = 0
+        for X_batch, y_batch in EVAL_GEN:
+            if i >= np.ceil(USEFULNESS_EVAL_SET_SIZE/BATCH_SIZE):
+                # print(f'breaking at epoch i: {i}')
+                break
+            value_list = session.run([total_loss, output, activations],
+                                     feed_dict={inputs: X_batch, labels: y_batch, mask: usefulness_mask})
+            (_total_loss, predicted, _activations) = value_list[:3]
+            _total_acc.append(accuracy(predicted, y_batch))
+            _total_losses.append(_total_loss)
+            i += 1
+
+    eval_loss = np.mean(_total_losses)
+    eval_acc = np.mean(_total_acc)
+
+    return eval_loss, eval_acc
+
+
+def accuracy(predicted, expected):
+    return float(np.sum(np.argmax(predicted, axis=1) == expected)) / len(predicted)
+
+
+def create_0_mask(dep, wid):
+    new_mask = np.ones((DEPTH, WIDTH))
+    new_mask[dep, wid] = 0
+    return(new_mask)
+
+
+def find_weights(d, w):
+    trainables = dict([(v.name, v) for v in tf.trainable_variables()])
+    input_weights = session.run(trainables[f"dense_{d}/kernel:0"])[:, w]
+    output_weights = session.run(trainables[f"dense_{d+1}/kernel:0"])[w, :]
+    return(input_weights, output_weights)
+
+
+cumulative_dictionary = {}
+
+start_time = time.time()
+iteration_no = 0
+saver = tf.train.Saver(max_to_keep=20)
+
+for iteration in range(ITERS+1):
+
+    # print(f'iteration number {iteration}')
+    train_data = next(train_gen)
+    # training step
+    _, _total_loss, predicted, loss_summary = session.run(
+        [optimizer, total_loss, output, merged_loss_summary_op],
+        feed_dict={inputs: train_data[0], labels: train_data[1], mask: dummy_mask}
+    )
+    log_writer.add_summary(loss_summary, iteration)
+    usefulness_dict = dict([(f"{d} {w}", []) for d in range(DEPTH) for w in range(WIDTH)])
+
+    # eval step
+    if iteration % EVALUATION_CHECKPOINT == 0:
+        train_acc = accuracy(predicted, train_data[1])
+        eval_loss, eval_acc, nonzeros, current_activations, current_zs, labels_evaluated =\
+            evaluate(X_devel, y_devel, BATCH_SIZE)
+
+        saver.save(session, f'saved_models/model_{SESSION_NAME}', global_step=iteration)
+        # print(_total_loss)
+        # print('Total loss:{:.3f}, L reg:{}'.format(_total_loss,
+        # session.run(reg_losses)))
+        # print(session.run([reg_losses]))
+        # print(tf.math.reduce_sum(reg_losses))
+        print("{:>5}:    train acc {:.2f}    dev acc {:.2f}".format(
+            iteration, train_acc, eval_acc))
+        # print(zs)
+        # print(f"HEREWEGO:{nonzeros}")
+
+        # for line in nonzeros:
+        #     print(' '.join(['{: <2}'.format(x) for x in line]))
+
+        # print(current_zs)
+        # print(len(X_devel))
+        zs_evaluated = np.empty((DEPTH, 1, WIDTH))
+        for i in range(0, len(X_devel), BATCH_SIZE):
+            current = [session.run([current_zs],
+                                   feed_dict={inputs: X_devel[i:i+BATCH_SIZE],
+                                              labels: y_devel[i:i+BATCH_SIZE],
+                                              mask: dummy_mask})]
+            current = np.squeeze(np.array(current))
+            zs_evaluated = np.concatenate((zs_evaluated, current), axis=1)
+
+        # zs_dict[iteration][depth][no_of_images][width]
+        # zs_dict[iteration_no] = zs_evaluated[:, 1:, :].tolist()
+        zs_dict[iteration_no] = zs_evaluated[:, 1:, :]
+
+        if EVALUATE_USEFULNESS:
+
+            usefulness_starttime = time.time()
+            EVAL_GEN = data.classifier_generator((X_devel, y_devel), BATCH_SIZE, infinity=True)
+            cumulative_dictionary[iteration_no] = {}
+
+            # print(f'length of devel: {len(X_devel)}')
+
+            for d in range(DEPTH):
+                for w in range(WIDTH):
+                    current_mask = create_0_mask(d, w)
+                    u_eval_loss, u_eval_acc = evaluate_usefulness(X_devel, y_devel, current_mask)
+                    if u_eval_acc == 0:
+                        usefulness_a = 0
+                    else:
+                        usefulness_a = eval_acc/u_eval_acc
+
+                    if eval_loss == 0:
+                        usefulness_l = 0
+                    else:
+                        usefulness_l = u_eval_loss/eval_loss
+
+                    current_neuron = f"{d} {w}"
+                    usefulness_dict[current_neuron] += [(usefulness_l, usefulness_a)]
+
+                    # print(f"Loc: ({d}, {w}), loss and acc: {u_eval_loss:.3f}, {u_eval_acc:.3f}")
+                    # print(f"Loc: ({d}, {w}), acc and loss usefulness: {usefulness_a:.3f}, {usefulness_l:.3f}")
+                    # print(f"Usefulness_in_a: {usefulness_a:.3f}")
+                    # print(f"Usefulness_in_l: {usefulness_l:.3f}")
+                    # if loss gets higher without neuron -> not useful = ratio < 1
+                    # if accuracy gets lower without neuron -> not useful (reciprocated) = ratio < 1
+                    usefulness_endtime = time.time()
+                    usefulness_elapsed = usefulness_endtime-usefulness_starttime
+                    in_w, ou_w = find_weights(d, w)
+                    # CUMULATIVE_DICT SECTION
+
+                    temp_cum = {'activations': zs_dict[iteration_no][d, :, w].tolist(),
+                                'usefulness_loss': usefulness_l.tolist(),
+                                'usefulness_acc': usefulness_a.tolist(),
+                                'depth': d,
+                                'inverse_depth': DEPTH-1-d,
+                                'width': w,
+                                'input_weights': in_w.tolist(),
+                                'output_weights': ou_w.tolist(),
+                                'reg_loss_in_layer': session.run(reg_losses)[d].tolist()}
+
+                    cumulative_dictionary[iteration_no][current_neuron] = temp_cum
+
+        if EVALUATE_USEFULNESS:
+
+            print(f"""Usefulness loop time: {usefulness_elapsed:.2f} seconds, with
+                  {usefulness_elapsed/(DEPTH*WIDTH):.2f} seconds per
+                  subloop.""")
+            cumulative_dictionary[iteration_no]['original_labels'] = list(
+                itertools.chain.from_iterable(labels_evaluated[0]))
+            cumulative_dictionary[iteration_no]['predicted_labels'] = list(
+                itertools.chain.from_iterable(labels_evaluated[1]))
+
+        iteration_no += 1
+
+        DEBUG = True
+
+
+print("Total time: {}".format(time.time() - start_time))
+
+if EVALUATE_USEFULNESS:
+    with open('neuron_logs/train_data/output_{}.json'.format(SESSION_NAME), 'w') as f:
+        json.dump(cumulative_dictionary, f)
